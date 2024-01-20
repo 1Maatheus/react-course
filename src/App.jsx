77aee8378862b6169beed52311e986e241824404
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-20 13:49:58 -0300

    Customizing components with tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Post } from "./components/Post";
 
 function App() {
   return (
-    <>
-      <div className="bg-gradient-to-b  from-[#321D51] to-[#6C5185] p-4 h-screen">
-        <Post />
-        <Post />
-      </div>
-    </>
+    <div className="bg-gradient-to-b text-center from-[#321D51] to-[#6C5185] py-4 h-screen flex flex-col gap-7 text-[2.5rem]">
+      <Post />
+      <Post />
+    </div>
   );
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export const Post = () => {
-  return <div className="text-center text-white">Post</div>;
+  return (
+    <div className="h-fit p-7 my-4 mx-0 bg-purple-200 rounded-lg shadow-[0_1px_4px_rgba(0,0,0,0.2)]">
+      <p className="text-[0.8rem] font-bold text-violet-900 m-0 uppercase">
+        Matheus
+      </p>
+      <p className="text-xl text-violet-500 whitespace-pre-wrap italic m-[0.25rem 0 0 0]">
+        Nayara
+      </p>
+    </div>
+  );
 };
